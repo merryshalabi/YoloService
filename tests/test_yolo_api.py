60f224c2cc59634204e105d434c00380ebaaeb17
merryshalabi <merryshalabi59@gmail.com>
2025-05-19 13:04:55 +0300

    deployment test

--- tests/test_yolo_api.py
+++ tests/test_yolo_api.py
@@ -91,6 +91,7 @@ class TestYoloAPI(unittest.TestCase):
         response = client.get(f"/image/original/{uid}.jpg")
         self.assertEqual(response.status_code, 200)
 
+
     def test_image_endpoint_predicted_valid(self):
         with open("tests/test_image.jpg", "rb") as image_file:
             response = client.post("/predict", files={"file": image_file})
